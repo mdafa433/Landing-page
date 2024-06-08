1aa28bde4bcbd03d6cbccb5f902b1d3b2a431c72
Alex <muhammaddafa6279@gmail.com>
2024-06-08 15:15:16 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         'biru' : '#0B63DC'
+      },
+      fontFamily: {
+        'viga' : ['viga']
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/img/jumbotron-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 }
-
